d73ae5c193fe532e33673a27ba4e2661d19e93a1
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-12-22 22:37:57 +0100

    minor: Start chord constructor functions

--- bin/chords.seeds.js
+++ bin/chords.seeds.js
@@ -1,9 +1,68 @@
 const Chord = require('../models/chord.model');
 
-const majorTriads = [
+const majorTriads = [];
+
+const NOTES = [
     {
-        name: 'C major',
-        symbol: 'C',
-        notes: ['C', 'E', 'G']
-    }
+        sharp: 'C',
+        flat: 'C',
+    },
+    {
+        sharp: 'C#',
+        flat: 'Db',
+    },
+    {
+        sharp: 'D',
+        flat: 'D',
+    },
+    {
+        sharp: 'D#',
+        flat: 'Eb',
+    },
+    {
+        sharp: 'E',
+        flat: 'E',
+    },
+    {
+        sharp: 'F',
+        flat: 'F',
+    },
+    {
+        sharp: 'G#',
+        flat: 'Ab',
+    },
+    {
+        sharp: 'A',
+        flat: 'A',
+    },
+    {
+        sharp: 'A#',
+        flat: 'Bb',
+    },
 ]
+
+const cMajor = {
+	name: 'C major',
+	symbol: 'C',
+	notes: ['C', 'E', 'G'],
+};
+
+const createMajTriad = (n, type) => {
+    const chord = {
+        name: `${NOTES[n][type]} major`,
+        symbol: `${NOTES[n][type]}`,
+        notes: [`NOTES[n][type]`],
+    }
+}
+
+const findChordNotes = (n, chordFormula, type) => {
+   if (n >= 12) {
+       n -= 12
+   }
+
+
+}
+
+const majChordFormula = () => {
+    // [n, n + 4, n + 7]
+}
